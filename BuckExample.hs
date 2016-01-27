fd57d6fbf67270848b92948848947db8dd9b985e
Daniel Martí <mvdan@mvdan.cc>
2016-01-27 15:20:41 +0000

    Get rid of all tabs

--- BuckExample.hs
+++ BuckExample.hs
@@ -1,5 +1,5 @@
 module BuckExample (
-	zcAbsentScenario
+    zcAbsentScenario
     ) where
 
 import Circuit
@@ -8,20 +8,20 @@ import Circuit.Concept
 zcAbsentScenario :: Eq a => a -> a -> a -> a -> a -> a -> a -> CircuitConcept a
 zcAbsentScenario uv oc zc gp gp_ack gn gn_ack = chargeFunc <> uvFunc <> uvReact <> zcAbsent
   where
-  	uvFunc = rise uv ~> rise gp <> rise uv ~> fall gn
-  	ocFunc = rise oc ~> fall gp <> rise oc ~> rise gn
+      uvFunc = rise uv ~> rise gp <> rise uv ~> fall gn
+      ocFunc = rise oc ~> fall gp <> rise oc ~> rise gn
 
-  	uvReact = rise gp_ack ~> fall uv <> fall gn_ack ~> fall uv
-  	ocReact = fall gp_ack ~> fall oc <> rise gn_ack ~> fall oc
+      uvReact = rise gp_ack ~> fall uv <> fall gn_ack ~> fall uv
+      ocReact = fall gp_ack ~> fall oc <> rise gn_ack ~> fall oc
 
-  	environmentConstraint = me uv oc
-  	circuitConstraint     = me gp gn
+      environmentConstraint = me uv oc
+      circuitConstraint     = me gp gn
 
-  	gpHandshake = handshake gp gp_ack
-  	gnHandshake = handshake gn gn_ack
+      gpHandshake = handshake gp gp_ack
+      gnHandshake = handshake gn gn_ack
 
-  	initialState = initialConcept $ before (rise uv) .&&. before (rise oc)
+      initialState = initialConcept $ before (rise uv) .&&. before (rise oc)
 
-	chargeFunc = ocFunc <> ocReact <> environmentConstraint <> circuitConstraint <> gpHandshake <> gnHandshake
+      chargeFunc = ocFunc <> ocReact <> environmentConstraint <> circuitConstraint <> gpHandshake <> gnHandshake
 
-	zcAbsent = silent (rise zc) <> silent (fall zc)
+      zcAbsent = silent (rise zc) <> silent (fall zc)
