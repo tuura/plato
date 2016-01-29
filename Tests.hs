fe424cac9f612b3f45a0f0cf5c95ce7352423c9a
Daniel Martí <mvdan@mvdan.cc>
2016-01-29 11:32:20 +0000

    Further simplify test body

--- Tests.hs
+++ Tests.hs
@@ -24,11 +24,15 @@ data Signal = A | B deriving (Eq, Show, Enum, Bounded)
 testBuffer = do
     putStrLn "=== testBuffer"
     assertEq True $ initial circuit initState
-    assertEq [rise A] $ enabledTransitions initState circuit
+    assertEq [rise A] $ enablTrans initState
+
     let afterA = fire (rise A) initState
-    assertEq [fall A, rise B] $ enabledTransitions afterA circuit
+    assertEq [fall A, rise B] $ enablTrans afterA
+
     let afterB = fire (rise B) afterA
-    assertEq [fall A] $ enabledTransitions afterB circuit
+    assertEq [fall A] $ enablTrans afterB
+
     where
         initState = State $ const False
         circuit = consistency <> buffer A B
+        enablTrans state = enabledTransitions state circuit
