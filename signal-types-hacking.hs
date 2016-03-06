c95907feb73580c4a2aeaf6d211312f93c4021a9
Andrey Mokhov <andrey.mokhov@gmail.com>
2016-03-06 00:14:34 +0000

    Add internal signals, improve naming.

--- signal-types-hacking.hs
+++ signal-types-hacking.hs
@@ -1,42 +1,52 @@
 {-# LANGUAGE DataKinds, GADTs, TypeFamilies #-}
 {-# OPTIONS_GHC -fno-warn-unticked-promoted-constructors #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 
-data SignalType = I | O
+-- Internal (I), output (O) and internal (T) signal types
+data SignalTag = I | O | T
 
 data Signal t a where
-    Input  :: a -> Signal I a
-    Output :: a -> Signal O a
+    Input    :: a -> Signal I a
+    Output   :: a -> Signal O a
+    Internal :: a -> Signal T a
 
-type InputSignal  = Signal I
-type OutputSignal = Signal O
+fromSignal :: Signal t a -> a
+fromSignal (Input    a) = a
+fromSignal (Output   a) = a
+fromSignal (Internal a) = a
+
+type Input    = Signal I
+type Output   = Signal O
+type Internal = Signal T
 
 data DependencyType = Causality | TimingAssumption | ConcurrencyReduction
 
-type family Dependency a b where
+type family Dependency (a :: SignalTag) (b :: SignalTag) where
     Dependency I I = TimingAssumption
-    Dependency I O = Causality
-    Dependency O I = Causality
     Dependency O O = ConcurrencyReduction
+    Dependency a b = Causality
 
 (~>) :: (Dependency t1 t2 ~ Causality) => Signal t1 a -> Signal t2 a -> (a, a)
-(~>) (Input x) (Output y) = (x, y)
-(~>) (Output x) (Input y) = (x, y)
-(~>) _ _ = error "impossible" -- This redundant case won't be needed in GHC 8.0
+(~>) x y = (fromSignal x, fromSignal y)
 
-a, b :: InputSignal String
+a, b :: Input String
 a = Input "a"
 b = Input "b"
-c :: OutputSignal String
+c :: Output String
 c = Output "c"
+t :: Internal String
+t = Internal "t"
 
-p_ac, p_cb :: (String, String)
-p_aa = a ~> a
+p_ac, p_cb, p_tt :: (String, String)
+-- p_aa = a ~> a
 -- p_ab = a ~> b
 p_ac = a ~> c
 p_cb = c ~> b
 -- p_cc = c ~> c
+p_tt = t ~> t
 
 main :: IO ()
 main = do
     putStrLn $ show p_ac
     putStrLn $ show p_cb
+    putStrLn $ show p_tt
