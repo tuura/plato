5e2c885afcfa9c7dbeaffa0a6a0eb00b68bda925
Daniel Martí <mvdan@mvdan.cc>
2016-05-17 14:18:09 +0100

    Always name signals A,B,C,...
    This means we no longer require `data Signal` to be defined in the
    user's concept module. Simplifies the code and removes the TODO.

--- examples/Concept.hs
+++ examples/Concept.hs
@@ -2,9 +2,6 @@ module Concept where
 
 import Tuura.Concept
 
--- Signals
-data Signal = A | B | C deriving (Eq, Show, Enum, Bounded)
-
 -- Example circuit described using gate-level concepts
 circuit :: (Eq a) => a -> a -> a -> CircuitConcept a
 circuit a b c = consistency <> cElement a b c <> inverter c a <> inverter c b
