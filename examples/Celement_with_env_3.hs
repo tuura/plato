20250016558355168f0bb02d70e66eb390e7e442
Jonny Beaumont <jonathan.r.beaumont@gmail.com>
2016-08-15 15:34:23 +0100

    Changed/added concepts for examples in the article

---
+++ examples/Celement_with_env_3.hs
@@ -0,0 +1,8 @@
+module Concept where
+
+import Tuura.ConceptConcat
+
+-- C-element with environment circuit described using protocol-level concepts
+circuit :: (Eq a) => a -> a -> a -> CircuitConcept a
+circuit a b c = handshake00 a c <> handshake00 b c
+
