328ae079d9ca40dc1a021c077240bbffc195adee
Jonny Beaumont <jonathan.r.beaumont@gmail.com>
2016-10-22 15:34:53 +0100

    Fixed TransTest to be cleaner

--- TransTest.hs
+++ TransTest.hs
@@ -18,61 +18,50 @@ testDotGOutput = do
     putStrLn "=== testDotGOutput"
     assertEq threeSignalsConcept expected
   where
-    expected = ".model out\n"          ++
-               ".inputs A\n"           ++
-               ".outputs B\n"          ++
-               ".internal C\n"         ++
-               ".graph\n"              ++
-               "A0 A+\n"               ++
-               "A+ A1\n"               ++
-               "A1 A-\n"               ++
-               "A- A0\n"               ++
-               "B0 B+\n"               ++
-               "B+ B1\n"               ++
-               "B1 B-\n"               ++
-               "B- B0\n"               ++
-               "C0 C+\n"               ++
-               "C+ C1\n"               ++
-               "C1 C-\n"               ++
-               "C- C0\n"               ++
-               ".marking {A0 B1 C0}\n" ++
-               ".end\n"
+    threeSignalsConcept = doTranslate signs (inputs [a] <> outputs [b] <> internals [c] <> initialise0 [a, c] <> initialise1 [b])
+    a = Signal 0
+    b = Signal 1
+    c = Signal 2
+    signs = [a, b, c]
+    expected = unlines ([".model out", ".inputs A", ".outputs B", ".internal C", ".graph"] ++
+                        ["A0 A+", "A+ A1", "A1 A-", "A- A0", "B0 B+", "B+ B1", "B1 B-", "B- B0"] ++
+                        ["C0 C+", "C+ C1", "C1 C-", "C- C0", ".marking {A0 B1 C0}", ".end"])
 
 testHandshakeConcept :: IO ()
 testHandshakeConcept = do
     putStrLn "===testHandshakeConcept"
-    assertEq (sort handshakeConcept) (sort expected)
+    assertEq (sort (arcs handshakeConcept)) (sort expected)
   where
+    handshakeConcept = inputs[a] <> outputs [b] <> handshake00 a b
     expected = [([rise a], rise b), ([rise b], fall a), ([fall a], fall b), ([fall b], rise a)]
-    a = Signal 0
-    b = Signal 1
+    [a, b] = map Signal [0 .. 1]
 
 testOrGateConcept :: IO ()
 testOrGateConcept = do
     putStrLn "===testOrGateConcept"
-    assertEq (sort orGateConcept) (sort expected)
+    assertEq (sort (arcs orGateConcept)) (sort expected)
   where
+    orGateConcept = inputs [a, b] <> outputs [c] <> initialise0 [a, b, c] <> orGate a b c
     expected = [([rise a, rise b], rise c), ([fall a], fall c), ([fall b], fall c)]
-    a = Signal 0
-    b = Signal 1
-    c = Signal 2
+    [a, b, c] = map Signal [0..2]
 
 testTwoAndGates :: IO ()
 testTwoAndGates = do
     putStrLn "===testTwoAndGates"
-    assertEq (sort twoAndGatesConcept) (sort expected)
+    assertEq (sort (arcs twoAndGatesConcept)) (sort expected)
   where
+    twoAndGatesConcept = inputs [a, b, c, d] <> outputs [out] <> initialise0 [a, b, c, d, out] <> andGate a b out <> andGate c d out
     expected = [([rise a], rise out), ([rise b], rise out), ([rise c], rise out), ([rise d], rise out), ([fall a, fall b], fall out), ([fall c, fall d], fall out)]
-    a   = Signal 0
-    b   = Signal 1
-    c   = Signal 2
-    d   = Signal 3
-    out = Signal 4
+    [a, b, c, d, out] = map Signal [0..4]
 
 testTwoDifferentCElements :: IO ()
 testTwoDifferentCElements = do
     putStrLn "===testTwoDifferentCElements"
-    assertEq (sort (arcs cElementConcept1)) (sort (arcs cElementConcept2))
+    assertEq (sort (arcs cElementConcept)) (sort (arcs cElementConceptFromBuffers))
+  where
+    cElementConcept = inputs [a, b] <> outputs [c] <> initialise0 [a, b, c] <> cElement a b c
+    cElementConceptFromBuffers = inputs [a, b] <> outputs [c] <> initialise0 [a, b, c] <> buffer a c <> buffer b c
+    [a, b, c] = map Signal [0..2]
 
 assertEq :: (Eq a, Show a) => a -> a -> IO ()
 assertEq have need
@@ -81,51 +70,3 @@ assertEq have need
         putStrLn $ "have: " ++ show have
         exitFailure
     | otherwise = putStrLn $ "OK " ++ show need
-
-threeSignalsConcept :: String
-threeSignalsConcept = doTranslate signs circuit
-  where
-    circuit = inputs [a] <> outputs [b] <> internals [c] <> initialise0 [a, c] <> initialise1 [b]
-    a = Signal 0
-    b = Signal 1
-    c = Signal 2
-    signs = [a, b, c]
-
-handshakeConcept :: [([Transition Signal], Transition Signal)]
-handshakeConcept = arcs circuit
-  where
-    circuit = inputs[a] <> outputs [b] <> handshake00 a b
-    a = Signal 0
-    b = Signal 1
-
-orGateConcept :: [([Transition Signal], Transition Signal)]
-orGateConcept = arcs circuit
-  where
-    circuit = inputs [a, b] <> outputs [c] <> initialise0 [a, b, c] <> orGate a b c
-    a = Signal 0
-    b = Signal 1
-    c = Signal 2
-
-twoAndGatesConcept :: [([Transition Signal], Transition Signal)]
-twoAndGatesConcept = arcs circuit
-  where
-    circuit = inputs [a, b, c, d] <> outputs [out] <> initialise0 [a, b, c, d, out] <> andGate a b out <> andGate c d out
-    a   = Signal 0
-    b   = Signal 1
-    c   = Signal 2
-    d   = Signal 3
-    out = Signal 4
-
-cElementConcept1 :: CircuitConcept Signal
-cElementConcept1 = inputs [a, b] <> outputs [c] <> initialise0 [a, b, c] <> buffer a c <> buffer b c
-  where
-    a = Signal 0
-    b = Signal 1
-    c = Signal 2
-
-cElementConcept2 :: CircuitConcept Signal
-cElementConcept2 = inputs [a, b] <> outputs [c] <> initialise0 [a, b, c] <> cElement a b c
-  where
-    a = Signal 0
-    b = Signal 1
-    c = Signal 2
